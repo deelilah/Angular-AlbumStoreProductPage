66c54a747128d224eb7198abdad977604a9aa12e
Delilah Rose <rose.delilahgesicho@gmail.com>
2019-11-07 17:31:58 +0300

    Completed module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,17 +5,27 @@ import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 import { ProductPageComponent } from './product-page/product-page.component';
-import { ProdunctDescriptionComponent } from './produnct-description/produnct-description.component';
 import { ProductDescriptionComponent } from './product-description/product-description.component';
+
 import {ProductService} from './product.service';
 import { ProductTracklistingComponent } from './product-tracklisting/product-tracklisting.component';
 import { ProductListComponent } from './product-list/product-list.component';
+import { RouterModule,Routes } from '@angular/router';
 
+
+const appRoutes: Routes = [
+  {
+    path:'products',component: ProductListComponent},
+   { path:'product/:id', component: ProductPageComponent},
+   { path:'', redirectTo: 'products',pathMatch: 'full'}
+
+
+];
 @NgModule({
   declarations: [
     AppComponent,
     ProductPageComponent,
-    ProdunctDescriptionComponent,
+    ProductDescriptionComponent,
     ProductDescriptionComponent,
     ProductTracklistingComponent,
     ProductListComponent
@@ -23,7 +33,8 @@ import { ProductListComponent } from './product-list/product-list.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [ProductService],
   bootstrap: [AppComponent]
