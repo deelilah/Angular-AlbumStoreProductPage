b3c8d4186253ef31e7c4fcbacbcf2217bc63ddf1
Delilah Rose <rose.delilahgesicho@gmail.com>
2019-11-07 15:00:11 +0300

    Completed module

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -3,12 +3,12 @@ import { Http ,Response } from '@angular/http';
 import 'rxjs/add/operator/map';
 @Injectable()
 export class ProductService {
-  private_albumUrl = '../assets/album.json';
+  private _albumUrl = '../assets/album.json';
 
   constructor(private _http: Http) { }
 
   getAlbum(id: number){
-    return this._http.get(this.albumUrl).map((response) => response.json());
+    return this._http.get(this._albumUrl).map((response) => response.json());
   }
 
 }
